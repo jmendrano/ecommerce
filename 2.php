c87db7ee019e916cd6523eb67cb4161abae317d1
Onaaaaa <74759890+jmendrano@users.noreply.github.com>
2023-03-18 23:11:08 +0800

    Part 2

--- 2.php
+++ 2.php
@@ -1,28 +1,21 @@
 <?php
 include_once "db_conn.php";
 
-
-if(isset($_POST['p_item_id'])){
+if(isset($_POST['new_item_name'])){
+    $p_item_name=$_POST['new_item_name'];
+    $p_item_price=$_POST['new_item_price'];
+    
     $table = "products";
-    
-    $p_item_id  = $_POST['p_item_id'];
-    $p_item_name = $_POST['p_item_name'];
-    $p_item_price = $_POST['p_item_price'];
-    
-    
-    $fields = array( 'item_name' => $p_item_name
+    $fields = array('item_name' => $p_item_name
                    , 'item_price' => $p_item_price
                    );
-    $filter = array( 'item_id' => $p_item_id );
     
-   
-   if( update($conn, $table, $fields, $filter )){
-       header("location: index.php?update_status=success");
-       exit();
-   }
+    if(insert($conn, $table, $fields) ){
+        header("location: index.php?new_record=added");
+        exit();
+    }  
     else{
-        header("location: index.php?update_status=failed");
+        header("location: index.php?new_record=failed");
         exit();
     }
- }
-?>
+}
